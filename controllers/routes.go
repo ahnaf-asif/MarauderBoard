b0c3c15a2303bd80571af9f105fec46f97ee407c
ahnaf-asif <ahnafshahriar92@gmail.com>
2025-04-15 06:59:17 +0600

    cleanup

--- controllers/routes.go
+++ controllers/routes.go
@@ -4,9 +4,7 @@ import (
 	"log"
 
 	"github.com/ahnafasif/MarauderBoard/controllers/auth"
-	"github.com/ahnafasif/MarauderBoard/database"
 	"github.com/ahnafasif/MarauderBoard/helpers"
-	"github.com/ahnafasif/MarauderBoard/models"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -16,9 +14,8 @@ func RegisterRoutes(app *fiber.App) {
 		if err != nil {
 			log.Println("User not found")
 		} else {
-			db_user, _ := models.GetUserByEmail(database.DB, user.Email)
 			return ctx.Render("index", fiber.Map{
-				"User": db_user,
+				"User": user,
 			}, "layouts/main")
 		}
 		return ctx.Render("index", fiber.Map{}, "layouts/main")
