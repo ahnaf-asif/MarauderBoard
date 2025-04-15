a9998ca24dacbd6dbce2c500142fca9e302d5cd5
ahnaf-asif <ahnafshahriar92@gmail.com>
2025-04-15 08:31:47 +0600

    Add layouts and auth middleware

--- controllers/routes.go
+++ controllers/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ahnafasif/MarauderBoard/controllers/auth"
 	"github.com/ahnafasif/MarauderBoard/helpers"
+	"github.com/ahnafasif/MarauderBoard/middlewares"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -19,6 +20,13 @@ func RegisterRoutes(app *fiber.App) {
 			}, "layouts/main")
 		}
 		return ctx.Render("index", fiber.Map{}, "layouts/main")
+	})
+
+	app.Get("/dashboard", middlewares.AuthMiddleware, func(ctx *fiber.Ctx) error {
+		user := ctx.Locals("User")
+		return ctx.Render("dashboard", fiber.Map{
+			"User": user,
+		}, "layouts/dashboard")
 	})
 
 	authGroup := app.Group("/auth")
