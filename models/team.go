d05e1d9674661a83ee238a355673324d2846bd0d
ahnaf-asif <ahnafshahriar92@gmail.com>
2025-04-19 12:53:07 +0600

    add model functions for project, task, team

--- models/team.go
+++ models/team.go
@@ -17,3 +17,63 @@ type Team struct {
 	ChatGroup   *ChatGroup `gorm:"foreignKey:ChatGroupId"`
 	ChatGroupId int
 }
+
+func AddNewTeam(db *gorm.DB, team *Team) (*Team, error) {
+	if err := db.Create(team).Error; err != nil {
+		return nil, err
+	}
+	return team, nil
+}
+
+func GetTeamById(db *gorm.DB, id int) (*Team, error) {
+	team := &Team{}
+	if err := db.Preload("Leader").
+		Preload("Users").
+		Preload("Projects").
+		Preload("ChatGroup").
+		First(team, id).Error; err != nil {
+		return nil, err
+	}
+	return team, nil
+}
+
+func GetTeamsByWorkspaceId(db *gorm.DB, workspaceId int) ([]*Team, error) {
+	teams := []*Team{}
+	if err := db.Preload("Leader").
+		Preload("Users").
+		Preload("Projects").
+		Preload("ChatGroup").
+		Where("workspace_id = ?", workspaceId).
+		Find(&teams).Error; err != nil {
+		return nil, err
+	}
+	return teams, nil
+}
+
+func GetTeamsByUserId(db *gorm.DB, user_id int) ([]*Team, error) {
+	teams := []*Team{}
+	if err := db.Preload("Leader").
+		Preload("Users").
+		Preload("Projects").
+		Preload("ChatGroup").
+		Joins("JOIN team_users ON teams.id = team_users.team_id").
+		Where("team_users.user_id = ?", user_id).
+		Find(&teams).Error; err != nil {
+		return nil, err
+	}
+	return teams, nil
+}
+
+func UpdateTeam(db *gorm.DB, team *Team) (*Team, error) {
+	if err := db.Save(team).Error; err != nil {
+		return nil, err
+	}
+	return team, nil
+}
+
+func DeleteTeam(db *gorm.DB, team *Team) error {
+	if err := db.Delete(team).Error; err != nil {
+		return err
+	}
+	return nil
+}
