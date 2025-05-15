4063bc376aeb90f99714aec4a02345a45436fcee
ahnaf-asif <ahnafshahriar92@gmail.com>
2025-05-15 06:33:17 +0600

    fix gantt, add app dashboard

--- controllers/dashboard/dashboard_controller.go
+++ controllers/dashboard/dashboard_controller.go
@@ -1,6 +1,9 @@
 package dashboard_controller
 
 import (
+	"github.com/ahnafasif/MarauderBoard/database"
+	"github.com/ahnafasif/MarauderBoard/helpers"
+	"github.com/ahnafasif/MarauderBoard/models"
 	load_locals "github.com/ahnafasif/MarauderBoard/utils"
 	"github.com/gofiber/fiber/v2"
 )
@@ -8,6 +11,11 @@ import (
 func RegisterDashboardController(app fiber.Router) {
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		data := load_locals.LoadLocals(ctx)
+		user, _ := helpers.GetAuthUserSessionData(ctx)
+
+		workspaces, _ := models.GetAllWorkspacesByUserId(database.DB, user.ID)
+		data["Workspaces"] = workspaces
+
 		return ctx.Render("dashboard", data, "layouts/dashboard")
 	})
 }
